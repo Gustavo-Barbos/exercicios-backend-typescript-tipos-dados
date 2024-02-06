b4a6cd2200423af4fbafa28885ce767a8da98e23
Gustavo-Barbos <gustavo.soares.gsb@gmail.com>
2024-02-06 11:41:24 -0300

    Conclusão do exercício 04

---
+++ 04/scripts.ts
@@ -0,0 +1,22 @@
+const etiquetar = (
+    produto: {
+        produto: string,
+        lote: number,
+        ano: number,
+        qtd: number
+    }): string[] => {
+
+    const resultado: string[] = [];
+
+    for (let i = 1; i <= produto.qtd; i++) {
+        resultado.push(`${produto.lote}-${produto.ano}-${i.toString().padStart(3, '0')}`);
+
+    }
+    return resultado;
+}
+console.log(etiquetar({
+    produto: 'CPU Dual Core 3.0GHZ',
+    lote: 321,
+    ano: 2022,
+    qtd: 11
+}));
